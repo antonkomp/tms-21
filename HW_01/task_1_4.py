85ed62c0844bafae7631cdd30d972a093cf45088
antonkomp <antonkomp@gmail.com>
2020-10-04 03:18:05 +0300

    Homework_01

---
+++ HW_01/task_1_4.py
@@ -0,0 +1,8 @@
+from math import sqrt
+
+first_number = 123
+second_number = 345
+average_arif = (first_number + second_number) / 2
+average_geom = sqrt(first_number * second_number)
+print('Среднее арифметическое чисел -', average_arif, '\n',
+	'Cреднее геометрическое чисел -', average_geom)
