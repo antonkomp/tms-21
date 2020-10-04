188acb88e6f9d4228d03a328363488f81ace00e0
antonkomp <antonkomp@gmail.com>
2020-10-05 00:39:27 +0300

    Edit task 7

--- HW_07/task_7_1.py
+++ HW_07/task_7_1.py
@@ -3,25 +3,23 @@ def inch_to_centimeter(inches):
     return cent
 
 
-input_inches = int(input())
-centimeters = inch_to_centimeter(input_inches)
-print(centimeters)
-
-
 def centimeter_to_inch(cent):
     inch = cent / 2.54
     return inch
-
-
-input_centimeters = int(input())
-inch = centimeter_to_inch(input_centimeters)
-print(inch)
 
 
 def mile_to_km(miles):
     km = miles * 1.60934
     return km
 
+
+input_inches = int(input())
+centimeters = inch_to_centimeter(input_inches)
+print(centimeters)
+
+input_centimeters = int(input())
+inch = centimeter_to_inch(input_centimeters)
+print(inch)
 
 input_miles = int(input())
 km = mile_to_km(input_miles)
