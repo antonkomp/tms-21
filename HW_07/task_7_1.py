1dd601e3dfb4db2b98f6f204a9c199ed4174c53a
antonkomp <antonkomp@gmail.com>
2020-10-04 04:33:36 +0300

    Homework_07

---
+++ HW_07/task_7_1.py
@@ -0,0 +1,28 @@
+def inch_to_centimeter(inches):
+    cent = inches * 2.54
+    return cent
+
+
+input_inches = int(input())
+centimeters = inch_to_centimeter(input_inches)
+print(centimeters)
+
+
+def centimeter_to_inch(cent):
+    inch = cent / 2.54
+    return inch
+
+
+input_centimeters = int(input())
+inch = centimeter_to_inch(input_centimeters)
+print(inch)
+
+
+def mile_to_km(miles):
+    km = miles * 1.60934
+    return km
+
+
+input_miles = int(input())
+km = mile_to_km(input_miles)
+print(km)
