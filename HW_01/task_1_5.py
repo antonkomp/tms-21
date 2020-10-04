3a3a88e83d0df18e93d16fa187c4fcfbd3d5d6ee
antonkomp <antonkomp@gmail.com>
2020-10-04 03:18:11 +0300

    Homework_01

---
+++ HW_01/task_1_5.py
@@ -0,0 +1,8 @@
+from math import sqrt
+
+first_leg = 14
+second_leg = 8
+hypotenuse = sqrt(pow(first_leg, 2) + pow(second_leg, 2))
+square = first_leg * second_leg / 2
+print('Гипотенуза треугольника -', hypotenuse)
+print('Площадь треугольника', square)
