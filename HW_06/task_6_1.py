683930d66482b26ded06473f05e49fee322be0c7
antonkomp <antonkomp@gmail.com>
2020-10-05 05:28:15 +0300

    Homework_06 task 2

--- HW_06/task_6_1.py
+++ HW_06/task_6_1.py
@@ -13,3 +13,13 @@ for i in range(matrix_row):
     print()
 print()
 
+
+# 2 task
+def max_el(matrix):
+    return max(map(max, matrix))
+
+
+max_elem = max_el(matrix)
+print('max element:', max_elem)
+print()
+
